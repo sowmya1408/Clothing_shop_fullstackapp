af629f42ff181994a19dd7f4fc771d5459885c52
sowmya1408 <sowmyamannemr@gmail.com>
2020-12-16 22:07:35 +0100

    added the backend API for the products with catergory

--- frontend/src/actions/getProductsWithCategory.js
+++ frontend/src/actions/getProductsWithCategory.js
@@ -1,13 +1,17 @@
 import { GET_CATEGORY_PRODUCTS } from "./types";
 
 export const getCategoryProducts = (category, gender) => {
-  return  async (dispatch) => {
+  return async (dispatch) => {
     try {
-      const fetchCategory =  await fetch(`http://localhost:8080/api/products/search?query=${category}`);
-      const fetchCategoryJson = await  fetchCategory.json();
+      const fetchCategory = await fetch(
+        `https://clothinshop-backend.herokuapp.com/api/products/search?query=${category}`
+      );
+      const fetchCategoryJson = await fetchCategory.json();
       dispatch({
         type: GET_CATEGORY_PRODUCTS,
-        categoryProducts: fetchCategoryJson.filter(product => product.gender === gender),
+        categoryProducts: fetchCategoryJson.filter(
+          (product) => product.gender === gender
+        ),
       });
     } catch (err) {
       console.log(err);
