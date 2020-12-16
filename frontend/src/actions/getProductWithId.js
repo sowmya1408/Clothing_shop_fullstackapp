7e086fa20cadc3b90e4ca1c56220ef6fedbd635a
sowmya1408 <sowmyamannemr@gmail.com>
2020-12-16 22:06:40 +0100

    added the backend API for the products with id

--- frontend/src/actions/getProductWithId.js
+++ frontend/src/actions/getProductWithId.js
@@ -1,17 +1,19 @@
-import {GET_PRODUCT_WITH_ID} from './types';
+import { GET_PRODUCT_WITH_ID } from "./types";
 
-export const getProductId = (id) =>{
-    return  async (dispatch) => {
-        try {
-          const fetchIdProduct =  await fetch(`http://localhost:8080/api/products/${id}`);
-          const fetchIdProductJson = await  fetchIdProduct.json();
-          dispatch({
-            type: GET_PRODUCT_WITH_ID,
-            productsId: fetchIdProductJson,
-            cart: fetchIdProductJson
-          });
-        } catch (err) {
-          console.log(err);
-        }
-      };
-}
+export const getProductId = (id) => {
+  return async (dispatch) => {
+    try {
+      const fetchIdProduct = await fetch(
+        `https://clothinshop-backend.herokuapp.com/api/products/${id}`
+      );
+      const fetchIdProductJson = await fetchIdProduct.json();
+      dispatch({
+        type: GET_PRODUCT_WITH_ID,
+        productsId: fetchIdProductJson,
+        cart: fetchIdProductJson,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
