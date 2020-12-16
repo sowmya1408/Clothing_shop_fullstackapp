701fa33849cdc67a3a9715fc0c4f09dd95f6e349
sowmya1408 <sowmyamannemr@gmail.com>
2020-12-16 22:07:04 +0100

    added the backend API for the products

--- frontend/src/actions/getProducts.js
+++ frontend/src/actions/getProducts.js
@@ -1,43 +1,44 @@
-import { GET_PRODUCTS_DATA,OPEN_EACH_PRODUCT,ADD_PRODUCT_BASKET } from "./types";
+import {
+  GET_PRODUCTS_DATA,
+  OPEN_EACH_PRODUCT,
+  ADD_PRODUCT_BASKET,
+} from "./types";
 
 export const getProducts = () => {
   return (dispatch) => {
-    return fetch('http://localhost:8080/api/products')
+    return fetch("https://clothinshop-backend.herokuapp.com/api/products")
       .then((res) => res.json())
       .then((data) => {
         dispatch({
           type: GET_PRODUCTS_DATA,
-          products: data
-      });
+          products: data,
+        });
       });
   };
 };
 
-
- 
-
 export const openEachProduct = (id) => {
-    return (dispatch) => {
-    return fetch(`http://localhost:8080/api/products/${id}`)
+  return (dispatch) => {
+    return fetch(`https://clothinshop-backend.herokuapp.com/api/products/${id}`)
       .then((res) => res.json())
       .then((data) => {
         dispatch(getProducts(data));
       });
-    }
-}
+  };
+};
 
 export const getEachProducts = (oneProduct) => {
-    return {
-        type: OPEN_EACH_PRODUCT,
-        product: oneProduct
-    }
-}
+  return {
+    type: OPEN_EACH_PRODUCT,
+    product: oneProduct,
+  };
+};
 
 export const addProductToBasket = () => {
-    return (dispatch) => {
-        console.log("adding to basket")
-        dispatch({
-            type: ADD_PRODUCT_BASKET
-        })
-    }
-}
+  return (dispatch) => {
+    console.log("adding to basket");
+    dispatch({
+      type: ADD_PRODUCT_BASKET,
+    });
+  };
+};
